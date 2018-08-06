d53f156823e3c026b8baf543a4686b4a599bd6b0
hamaadshah <hamaadshah@hotmail.com>
2018-08-06 19:14:16 +0100

    Completed market risk management code.

--- R/market_risk.R
+++ R/market_risk.R
@@ -1,5 +1,8 @@
+options(scipen = 999)
+
 library(ggplot2)
 library(quantmod)
+library(MASS)
 
 start <- as.Date("2016-01-01")
 end <- as.Date("2016-12-31")
@@ -29,26 +32,46 @@ colnames(port.data) <- "Portfolio.ret"
 
 ggplot(data = port.data, 
        aes(x = Portfolio.ret)) +
-geom_density()
+geom_density(fill = "skyblue", alpha = 0.25)
 
-tail.risks <- quantile(x = port.data$Portfolio.ret, 
+tail.risks <- quantile(x = sample(x = port.data$Portfolio.ret, size = 100000, replace = TRUE), 
                        probs = c(0.01, 0.99))
+
+est.ret.vector <- colMeans(x = ret.data)
 
 est.sd <- as.matrix(apply(X = ret.data, MARGIN = 2, FUN = sd))
 
 est.cor.matrix <- cor(x = ret.data)
 
 est.cov.matrix <- est.cor.matrix * (est.sd %*% t(est.sd))
-s
-sqrt(t(as.matrix(rep(x = 1 / ncol(ret.data), ncol(ret.data)))) %*% 
+
+(sqrt(t(as.matrix(rep(x = 1 / ncol(ret.data), ncol(ret.data)))) %*% 
        est.cov.matrix %*% 
        as.matrix(rep(x = 1 / ncol(ret.data), ncol(ret.data)))) *
 qnorm(p = 0.01, 
       mean = 0, 
-      sd = 1)
+      sd = 1)) +
+mean(port.data$Portfolio.ret)
 
 qnorm(p = 0.01, 
       mean = mean(port.data$Portfolio.ret), 
       sd = sqrt(t(as.matrix(rep(x = 1 / ncol(ret.data), ncol(ret.data)))) %*% 
                   est.cov.matrix %*% 
                   as.matrix(rep(x = 1 / ncol(ret.data), ncol(ret.data)))))
+
+sim.ret.data <- mvrnorm(n = 100000, 
+                        mu = est.ret.vector, 
+                        Sigma = est.cov.matrix)
+sim.port.data <- data.frame(apply(X = sim.ret.data, 
+                                  MARGIN = 1,
+                                  FUN = function(x) weighted.mean(x = x, w = rep(x = 1 / ncol(sim.ret.data), ncol(sim.ret.data)))))
+
+colnames(sim.port.data) <- "Portfolio.ret"
+
+ggplot(data = sim.port.data, 
+       aes(x = Portfolio.ret)) +
+  geom_density(fill = "skyblue", alpha = 0.25)
+
+sim.tail.risks <- quantile(x = sim.port.data$Portfolio.ret, 
+                           probs = c(0.01, 0.99))
+
